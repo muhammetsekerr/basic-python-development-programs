68301d14763cf9f1b424658c1ec777ffb084237d
Muhammet Şeker <muhammetsekeerr@gmail.com>
2024-02-12 13:26:08 +0300

    Created KimlikKarti project

---
+++ KimlikKarti.py
@@ -0,0 +1,9 @@
+isim = "Muhammet"
+soyisim= "Şeker"
+dYeri= "Türkiye"
+dTarihi= "1990"
+
+print("İsim     :", isim)
+print("Soyisim  :", soyisim)
+print("D.Yeri   :", dYeri)
+print("D.Tarihi :", dTarihi)
