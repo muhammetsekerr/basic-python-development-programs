a41dfcb0b9d64881118bf2fb8ab3afd14fb0ce92
Muhammet Şeker <muhammetsekeerr@gmail.com>
2024-02-15 15:28:51 +0300

    Created BasitHesapMakinesi.py

---
+++ BasitHesapMakinesi.py
@@ -0,0 +1,7 @@
+sayi1 = float(input("Birinci Sayı: "))
+sayi2 = float(input("İkinci Sayı: "))
+
+print("Toplama Sonucu: ", sayi1+sayi2)
+print("Çıkarma Sonucu: ", sayi1-sayi2)
+print("Çarpma Sonucu: ", sayi1*sayi2)
+print("Bölme Sonucu: ", sayi1/sayi2)
